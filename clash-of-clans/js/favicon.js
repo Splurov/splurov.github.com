1d785f930a5d0b7318c849d601068e35faf3a38b
Mikhail Kalashnik <m.kalashnik@hh.ru>
2013-04-11 02:02:06 +0400

    clash of clans, renamed favicons to prevent caching, changelog updated

--- clash-of-clans/js/favicon.js
+++ clash-of-clans/js/favicon.js
@@ -1,6 +1,6 @@
 // (c) http://www.jonathantneal.com/blog/understand-the-favicon/
 navigator.appName == "Microsoft Internet Explorer" && (function (i, d, s, l) {
-    i.src = "/clash-of-clans/i/favicon.ico";
+    i.src = "/clash-of-clans/i/favicon-v2.ico";
     s = d.getElementsByTagName("script")[0];
     l = s.parentNode.insertBefore(d.createElement("link"), s);
     l.rel = "shortcut icon";
