cba2c69c6aa9f0c9c4901d02f8b03892e735b87c
Mikhail Kalashnik <m.kalashnik@hh.ru>
2013-08-18 23:20:46 +0400

    clash of clans - remove converting

--- clash-of-clans/js/all.js
+++ clash-of-clans/js/all.js
@@ -1,7 +1,6 @@
 /* build:js:js/mk.common.js */
 /* build:js:js/mk.calc.common.js */
 /* build:js:js/mk.calc.menu.js */
-/* build:js:js/mk.calc.converter.js */
 /* build:js:js/mk.calc.calculate.js */
 /* build:js:js/mk.calc.saved.js */
 /* build:js:js/mk.calc.share.js */
