6779a45482c3765393fe88f29ad7c47d693d39b2
Mikhail Kalashnik <m.kalashnik@hh.ru>
2014-01-05 20:29:29 +0400

    clash of clans - add to home screen without js; removed global window.mkIsIos

--- clash-of-clans/js/detect-mobile.js
+++ clash-of-clans/js/detect-mobile.js
@@ -1,12 +1,12 @@
 (function() {
     'use strict';
 
-    window.mkIsIos = !!navigator.userAgent.match(/(?:iPad|iPhone|iPod)/i);
-    window.mkIsMobile = window.mkIsIos || (!!navigator.userAgent.match(/(?:Android|IEMobile)/i));
+    var ios = !!navigator.userAgent.match(/(?:iPad|iPhone|iPod)/i);
+    window.mkIsMobile = ios || (!!navigator.userAgent.match(/(?:Android|IEMobile)/i));
 
     document.documentElement.classList.add((window.mkIsMobile ? 'mobile' : 'not-mobile'));
 
-    if (window.mkIsIos) {
+    if (ios) {
         document.documentElement.classList.add('ios');
     }
 
