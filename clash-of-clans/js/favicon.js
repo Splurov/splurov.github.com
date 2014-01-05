d4d63992f5255924fb1754ac62300beeb3b11d0e
Mikhail Kalashnik <m.kalashnik@hh.ru>
2014-01-05 20:05:50 +0400

    clash of clans - remove ico version of favicon

--- clash-of-clans/js/favicon.js
+++
@@ -1,8 +0,0 @@
-// (c) http://www.jonathantneal.com/blog/understand-the-favicon/
-navigator.appName == "Microsoft Internet Explorer" && (function (i, d, s, l) {
-    i.src = "/clash-of-clans/i/favicon-v2.ico";
-    s = d.getElementsByTagName("script")[0];
-    l = s.parentNode.insertBefore(d.createElement("link"), s);
-    l.rel = "shortcut icon";
-    l.href = i.src;
-})(new Image, document);
