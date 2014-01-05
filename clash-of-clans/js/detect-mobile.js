0835bd4ecb5b5eaf62ea0fcbf8f60d2deee042cf
Mikhail Kalashnik <m.kalashnik@hh.ru>
2014-01-05 20:01:31 +0400

    clash of clans - detect mobile moved to head and simplified

--- clash-of-clans/js/detect-mobile.js
+++ clash-of-clans/js/detect-mobile.js
@@ -2,16 +2,9 @@
     'use strict';
 
     window.mkIsIos = !!navigator.userAgent.match(/(?:iPad|iPhone|iPod)/i);
-    var otherMobile = !!navigator.userAgent.match(/(?:Android|IEMobile)/i);
-    window.mkIsMobile = window.mkIsIos || otherMobile;
+    window.mkIsMobile = window.mkIsIos || (!!navigator.userAgent.match(/(?:Android|IEMobile)/i));
 
-    var specialClass;
-    if (window.mkIsMobile) {
-        specialClass = 'mobile';
-    } else {
-        specialClass = 'not-mobile';
-    }
-    document.documentElement.classList.add(specialClass);
+    document.documentElement.classList.add((window.mkIsMobile ? 'mobile' : 'not-mobile'));
 
     if (window.mkIsIos) {
         document.documentElement.classList.add('ios');
