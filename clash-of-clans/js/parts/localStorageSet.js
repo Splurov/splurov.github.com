e65cb4cf567a4ba363f3f00012e634878074fab6
Mikhail Kalashnik <m.kalashnik@hh.ru>
2014-07-03 22:10:16 +0400

    clash of clans - special message for safari private browsing

--- clash-of-clans/js/parts/localStorageSet.js
+++ clash-of-clans/js/parts/localStorageSet.js
@@ -46,20 +46,31 @@ part('localStorageSet', [
             try {
                 localStorage.setItem(key, value);
             } catch (e) {
-                message = '<strong>Attention!</strong> Looks like we have exceeded the quota to store Clash Calc data.';
                 if (favoritesCount > 0) {
                     goal.reach('QUOTA_EXCEEDED_AGAIN', {'quotaExceededAgainFavoritesCount': favoritesCount.toString()});
-                    message += ' Please remove unused army compositions from favorites.';
+                    message = '<strong>Attention!</strong> Looks like we have exceeded the quota' +
+                              ' to store Clash Calc data. Please remove unused army compositions from favorites.';
                 } else {
                     var dataLength = -1;
                     var data = localStorage.getItem(localStorageKnownKeys[0]);
                     if (data) {
                         dataLength = data.length;
                     }
+
+                    if (dataLength === -1 && navigator.userAgent.indexOf('Safari') !== -1) {
+                        message = 'Looks like you are using private mode of the Safari browser, so it’s not possible' +
+                                  ' to store Clash Calc data. Please turn off private mode if you want to preserve data' +
+                                  ' between visits.';
+                        dataLength = -2;
+                    } else {
+                        message = '<strong>Attention!</strong> Looks like we have exceeded the quota' +
+                                  ' to store Clash Calc data.' +
+                                  ' Normally this shouldn’t have happened.' +
+                                  ' Perhaps your browser is configured in a special way.' +
+                                  ' To fix the problem please check the settings related to the Local Storage.';
+                    }
+
                     goal.reach('QUOTA_EXCEEDED_UNKNOWN', {'quotaExceededDataLength': dataLength.toString()});
-                    message += ' Normally this shouldn’t have happened.' +
-                               'Perhaps your browser is configured in a special way.' +
-                               'To fix the problem please check the settings related to the Local Storage.';
                 }
             }
         }
